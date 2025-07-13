83f8697149860785eec579630565b6df275b0dae
aegidos <andi.schmid1210@gmail.com>
2025-07-13 21:50:28 +0200

    Remove netlify.toml - configure in Netlify dashboard instead

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
