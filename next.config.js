4ddb1a25c46c1d8b7a4111e7b923390ef5c2316a
aegidos <andi.schmid1210@gmail.com>
2025-07-13 22:19:37 +0200

    Add public folder to Git

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Enables static export
-  trailingSlash: true, // Adds trailing slashes to URLs for static export compatibility
+  trailingSlash: true,
   images: {
-    unoptimized: true, // Ensures images work with static export
+    unoptimized: true,
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
