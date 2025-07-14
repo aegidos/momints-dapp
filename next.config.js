a2aae24f8a894dc61ced7a1fc8a84307ff3197c9
aegidos <andi.schmid1210@gmail.com>
2025-07-14 17:34:33 +0200

    Fix Next.js configuration and remove static HTML

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-};
-
-module.exports = {
-	env: {
+	trailingSlash: true,
+	images: {
+		unoptimized: true,
+	},
+	webpack: (config) => {
+		config.resolve.fallback = { fs: false, net: false, tls: false };
+		return config;
 	},
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import '../styles/globals.css'  // Import global styles
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
